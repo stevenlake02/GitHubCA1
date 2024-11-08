5193e104f787b6b26140b4d4f4481abec5d1146e
steven <122418634@umail.ucc.ie>
2024-11-08 11:17:43 +0000

    Corrected mathematical operators

--- mymath.py
+++ mymath.py
@@ -1,19 +1,19 @@
 def add_three(a, b, c):
-    return -1
+    return a+b+c
 
 def subtract_three(a, b, c):
-    return -1
+    return a-b-c
 
 def multiply_three(a, b, c):
-    return -1
+    return a*b*c
 
 def divide_three(a, b, c):
-    return -1
+    return a/b/c
 
 def square(a):
     #return a**2
-    return -1
+    return a**2
 
 def cube(a):
     #return a**3
-    return -1
+    return a**3
